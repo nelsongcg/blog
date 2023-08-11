411d330ab8cf38b89d6234502ddd5a0b3f41dfe8
Nelson Guimaraes <nelsongcg@Nelsons-MacBook-Air.local>
2023-08-11 15:25:09 +0900

    index page

--- blog/blog/settings/base.py
+++ blog/blog/settings/base.py
@@ -48,7 +48,7 @@ ROOT_URLCONF = "blog.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR,'templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -115,3 +115,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
